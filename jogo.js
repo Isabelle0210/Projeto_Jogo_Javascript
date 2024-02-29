e0c752254be4f45d0f493575ed1be3c4bc3e55c4
Isabelle Caroline <isabelleisa646@gmail.com>
2024-02-29 10:14:12 -0300

    parte 3
    mudando o lado da cara do mosquito + funções css

--- jogo.js
+++ jogo.js
@@ -15,7 +15,7 @@ function posicaoRandomica(){// foi necessario colocar dentro da função para qu
 //math.random gera valores aleatorios que vao de 0 a 1 
 //math.floor, faz o arredondamento do valor retirando casas decimais
     posicaoX = posicaoX < 0 ? 0 : posicaoX
-    //posicaoX é menor que zero. Se for verdadeiro, atribui-se o valor 0 a posicaoX; caso contrário, mantém-se o valor atual de posicaoX. Em outras palavras, essa linha assegura que posicaoX não seja negativo. Se posicaoX for menor que zero, é definido como zero.
+    //posicaoX é menor que zero. Se for verdadeiro, atribui-se o valor 0 a posicaoX; caso contrário, mantém-se o valor atual de posicaoX. Em outras palavras, essa linha assegura que posicaoX não seja negativo. Se posicaoX for menor que zero, é definido como zero. 
     posicaoY= posicaoY < 0 ? 0: posicaoY
 
     console.log(posicaoX, posicaoY)
@@ -23,10 +23,30 @@ function posicaoRandomica(){// foi necessario colocar dentro da função para qu
     var mosquito = document.createElement('img')//criado como uma variavel pq é necessario interligar ele como elemento filho do body
 //adicionando um filho para o body
     mosquito.src ='imagens/mosca.png'//definimos uma imagem para a variavel mosquito
-    mosquito.className = 'mosquito1'//A classNamepropriedade define ou retorna o atributo de classe de um elemento.
+    mosquito.className = tamanhoAleatorio() + ladoAleatorio()//A className propriedade define ou retorna o atributo de classe de um elemento.
     mosquito.style.left = posicaoX+'px'//estou atribuindo um elemento css para a posicaoX para q ela seja exibida em pixels
     mosquito.style.top = posicaoY+'px'
     //para que isso aconteca deve colocar a posição como absoluta
     mosquito.style.position = 'absolute'
     document.body.appendChild(mosquito)
+}
+function tamanhoAleatorio(){
+    var classe =Math.floor(Math.random() * 3) 
+    switch(classe){
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
+function ladoAleatorio(){
+    var classe =Math.floor(Math.random() * 2) //valores de 0,1 e proximo de 2
+    switch(classe){
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
 }
