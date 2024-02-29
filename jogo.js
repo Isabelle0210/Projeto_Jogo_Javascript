022c1714bead802092cf42f546065411c089b3f0
Isabelle Caroline <isabelleisa646@gmail.com>
2024-02-29 09:24:57 -0300

    Parte 2
    inclusão da ação de fazer o mosquito se mexer pela tela

--- jogo.js
+++ jogo.js
@@ -5,8 +5,28 @@ var largura= 0
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight //propriedade retorna a altura da área de conteúdo de uma janela.
     largura = window.innerWidth //propriedade retorna a largura da área de conteúdo de uma janela.
-    console.log(altura,largura)
+    //console.log(largura,altura)
 }
 ajustaTamanhoPalcoJogo()//aqui estou chamando a função
 
-console.log(altura,largura)
+function posicaoRandomica(){// foi necessario colocar dentro da função para que ela seja representada a hora que o body for carregado 
+    var posicaoX= Math.floor(Math.random() * largura)- 90
+    var posicaoY= Math.floor(Math.random() * altura)- 90 
+//math.random gera valores aleatorios que vao de 0 a 1 
+//math.floor, faz o arredondamento do valor retirando casas decimais
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    //posicaoX é menor que zero. Se for verdadeiro, atribui-se o valor 0 a posicaoX; caso contrário, mantém-se o valor atual de posicaoX. Em outras palavras, essa linha assegura que posicaoX não seja negativo. Se posicaoX for menor que zero, é definido como zero.
+    posicaoY= posicaoY < 0 ? 0: posicaoY
+
+    console.log(posicaoX, posicaoY)
+//cria elemento html
+    var mosquito = document.createElement('img')//criado como uma variavel pq é necessario interligar ele como elemento filho do body
+//adicionando um filho para o body
+    mosquito.src ='imagens/mosca.png'//definimos uma imagem para a variavel mosquito
+    mosquito.className = 'mosquito1'//A classNamepropriedade define ou retorna o atributo de classe de um elemento.
+    mosquito.style.left = posicaoX+'px'//estou atribuindo um elemento css para a posicaoX para q ela seja exibida em pixels
+    mosquito.style.top = posicaoY+'px'
+    //para que isso aconteca deve colocar a posição como absoluta
+    mosquito.style.position = 'absolute'
+    document.body.appendChild(mosquito)
+}
