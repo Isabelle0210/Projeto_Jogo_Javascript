7a2ab27be01a3e6b0ea124716936c6ce7244e476
Isabelle Caroline <isabelleisa646@gmail.com>
2024-02-29 11:57:28 -0300

    parte 5
    algumas alterações e sistema de vidas

--- jogo.js
+++ jogo.js
@@ -1,7 +1,7 @@
 var altura= 0
 var largura= 0
 //criamos a variavel fora da função para que dentro da função essas variaveis sejam afetadas
-
+var vidas = 1
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight //propriedade retorna a altura da área de conteúdo de uma janela.
     largura = window.innerWidth //propriedade retorna a largura da área de conteúdo de uma janela.
@@ -13,8 +13,14 @@ function posicaoRandomica(){// foi necessario colocar dentro da função para qu
     //removendo mosquito anterior caso exista
     if(document.getElementById('mosquito')){
         document.getElementById('mosquito'). remove()
+        if(vidas>3){
+            alert('interrompa')
+        }else{
+            document.getElementById('v'+ vidas).src ="imagens/coracao_vazio.png"
+            vidas ++
+        }
+        
     }
-    
     var posicaoX= Math.floor(Math.random() * largura)- 90
     var posicaoY= Math.floor(Math.random() * altura)- 90 
 //math.random gera valores aleatorios que vao de 0 a 1 
@@ -23,22 +29,26 @@ function posicaoRandomica(){// foi necessario colocar dentro da função para qu
     //posicaoX é menor que zero. Se for verdadeiro, atribui-se o valor 0 a posicaoX; caso contrário, mantém-se o valor atual de posicaoX. Em outras palavras, essa linha assegura que posicaoX não seja negativo. Se posicaoX for menor que zero, é definido como zero. 
     posicaoY= posicaoY < 0 ? 0: posicaoY
 
-    console.log(posicaoX, posicaoY)
 //cria elemento html
     var mosquito = document.createElement('img')//criado como uma variavel pq é necessario interligar ele como elemento filho do body
 //adicionando um filho para o body
     mosquito.src ='imagens/mosca.png'//definimos uma imagem para a variavel mosquito
-    mosquito.className = tamanhoAleatorio() + ladoAleatorio()//A className propriedade define ou retorna o atributo de classe de um elemento.
+    mosquito.className = tamanhoAleatorio() +' '+ ladoAleatorio()//A className propriedade define ou retorna o atributo de classe de um elemento.
     mosquito.style.left = posicaoX+'px'//estou atribuindo um elemento css para a posicaoX para q ela seja exibida em pixels
     mosquito.style.top = posicaoY+'px'
     //para que isso aconteca deve colocar a posição como absoluta
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+
+        document.getElementById('v1').src="imagens/coracao_vazio.png"
+    }
 
     document.body.appendChild(mosquito)
 }
 function tamanhoAleatorio(){
-    var classe =Math.floor(Math.random() * 3) 
+    var classe = Math.floor(Math.random() * 3) 
     switch(classe){
         case 0:
             return 'mosquito1'
@@ -49,7 +59,7 @@ function tamanhoAleatorio(){
     }
 }
 function ladoAleatorio(){
-    var classe =Math.floor(Math.random() * 2) //valores de 0,1 e proximo de 2
+    var classe = Math.floor(Math.random() * 2) //valores de 0,1 e proximo de 2
     switch(classe){
         case 0:
             return 'ladoA'
