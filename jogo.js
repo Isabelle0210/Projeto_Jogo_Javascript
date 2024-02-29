b94939aa1c0db8eec2795b2398af1945af22c3ca
Isabelle Caroline <isabelleisa646@gmail.com>
2024-02-29 15:54:42 -0300

    Nivel de dificuldade

--- jogo.js
+++ jogo.js
@@ -11,9 +11,9 @@ nivel = nivel.replace('?', ' ')
 if(nivel==='normal'){
     criaMosquitoTempo = 1500
 }else if(nivel ==='dificil'){
-    criaMosquitoTempo = 1000
-}else if(nivel ==='chucknorris'){
     criaMosquitoTempo = 500
+}else if(nivel ==='chucknorris'){
+    criaMosquitoTempo = 100
 }
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight //propriedade retorna a altura da área de conteúdo de uma janela.
