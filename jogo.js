e6f7ae4472477c758e85d55a9e123aa182142863
Isabelle Caroline <isabelleisa646@gmail.com>
2024-02-29 14:42:57 -0300

    Jogo completo

--- jogo.js
+++ jogo.js
@@ -2,12 +2,37 @@ var altura= 0
 var largura= 0
 //criamos a variavel fora da função para que dentro da função essas variaveis sejam afetadas
 var vidas = 1
+var tempo = 20
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', ' ')
+
+if(nivel==='normal'){
+    criaMosquitoTempo = 1500
+}else if(nivel ==='dificil'){
+    criaMosquitoTempo = 1000
+}else if(nivel ==='chucknorris'){
+    criaMosquitoTempo = 500
+}
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight //propriedade retorna a altura da área de conteúdo de uma janela.
     largura = window.innerWidth //propriedade retorna a largura da área de conteúdo de uma janela.
     //console.log(largura,altura)
 }
 ajustaTamanhoPalcoJogo()//aqui estou chamando a função
+var cronometro = setInterval( function(){
+    tempo -= 1
+    
+    if(tempo<0){
+        clearInterval(cronometro)
+        clearInterval(criaMosca)
+        window.location.href = "vitoria.html"
+    }else{
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+    
+},1000)
 
 function posicaoRandomica(){// foi necessario colocar dentro da função para que ela seja representada a hora que o body for carregado 
     //removendo mosquito anterior caso exista
